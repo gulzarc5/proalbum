70c322b711a967414d7257ec573f49e427335a27
imvishalnag <53851269+imvishalnag@users.noreply.github.com>
2020-02-12 15:02:03 +0530

    single product

--- resources/views/web/shop-single.blade.php
+++ resources/views/web/shop-single.blade.php
@@ -55,9 +55,9 @@
                                         </div><!-- end shopmeta -->
                                     </div>
                                     <div id="product-select">
-                                        <h4>Product Selection</h4>
+                                        <h4 style="margin-bottom: 0">Product Selection</h4><hr>
                                         <div class="row">
-                                            <div class="form-group col-sm-6">
+                                            <div class="form-group col-sm-4">
                                                 <label>Size*</label>
                                                 <select class="form-control">
                                                     <option value="">12 x 12</option>
@@ -65,14 +65,48 @@
                                                     <option value="">20 x 20</option>                                                    
                                                 </select>
                                             </div>
-                                            <div class="form-group col-sm-6">
+                                            <div class="form-group col-sm-4">
                                                 <label>Number of pages*</label>
                                                 <input type="text" class="form-control" placeholder="">
+                                            </div>
+                                            <div class="form-group col-sm-4">
+                                                <label>Product Type*</label>
+                                                <select class="form-control">
+                                                    <option value="">Paper</option>
+                                                    <option value="">Spread</option>
+                                                    <option value="">Quantity</option>                                                    
+                                                </select>
+                                            </div>
+                                        </div>
+                                        <div class="row my-option">
+                                            <div class="form-group col-sm-12"><h5><strong>Select Option</strong></h5></div>
+                                            <div class="form-group col-sm-4">
+                                                <label>Colour*</label>
+                                                <select class="form-control" id="myselection">
+                                                    <option>Select Option</option>
+                                                    <option value="One"/>Manager</option>
+                                                    <option value="Two"/>HR</option>
+                                                    <option value="Three"/>Developer</option>
+                                                </select>
+                                            </div>
+                                            <div class="form-group col-sm-4">
+                                                <div id="showOne" class="myDiv">
+                                                    <img src="{{asset('web/upload/shop_02.jpg')}}" alt="Manager" class="img-responsive img-thumbnail"/>
+                                                </div>
+                                                <div id="showTwo" class="myDiv">
+                                                    <img src="{{asset('web/upload/shop_01.jpg')}}" alt="HR" class="img-responsive img-thumbnail"/>
+                                                </div>
+                                                <div id="showThree" class="myDiv">
+                                                    <img src="{{asset('web/upload/shop_03.jpg')}}" alt="Developer" class="img-responsive img-thumbnail"/>
+                                                </div>
                                             </div>
                                         </div>
                                         <div class="flex">
                                             <a href="{{route('web.shipping_address_list')}}" class="btn btn-cancel" style="margin-right:10px">Cancel</a>
                                             <button type="submit" class="btn btn-primary">Proceed to cart</button>                                
+                                        </div>
+                                        <div class="price-div">
+                                            <h5>Sub Total : R 100</h5>
                                         </div>
                                     </div>   
                                 </div><!-- end desc -->
@@ -211,5 +245,15 @@
             jQuery(this).attr('rel', jQuery(this).data('gal')); });     
             jQuery("a[data-rel^='prettyPhoto']").prettyPhoto({animationSpeed:'slow',theme:'light_square',slideshow:true,overlay_gallery: true,social_tools:false,deeplinking:false});
             })(jQuery);
+        </script>
+
+        <script>
+            $(document).ready(function(){
+                $('#myselection').on('change', function(){
+                    var demovalue = $(this).val(); 
+                    $("div.myDiv").hide();
+                    $("#show"+demovalue).show();
+                });
+            });
         </script>
     @endsection     
