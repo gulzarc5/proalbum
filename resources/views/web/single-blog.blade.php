1d154f8c02b8b7b6c77769d0cebb4a146b5b975d
imvishalnag <imvishalnag@gmail.com>
2020-07-03 14:49:47 +0530

    update

--- resources/views/web/single-blog.blade.php
+++ resources/views/web/single-blog.blade.php
@@ -45,8 +45,8 @@
                                   <!-- facebook -->
                                   <a class="facebook" href="https://www.facebook.com/share.php?u={{route('web.single-blog',['slug'=>$blog->slug,'b_id'=>$blog->id])}}&title={{$blog->title}}" target="blank"><i class="fa fa-facebook"></i></a> 
                                   <!-- twitter -->
-                                   <a class="twitter" href="https://twitter.com/intent/tweet?status={{$blog->title}}+{{route('web.single-blog',['slug'=>$blog->slug,'b_id'=>$blog->id])}}" target="blank"><i class="fa fa-twitter"></i></a> 
-                                  <!-- google plus -->
+                                   <a class="twitter" href="https://twitter.com/share?url={{route('web.single-blog',['slug'=>$blog->slug,'b_id'=>$blog->id])}}&text={{$blog->title}}" target="blank"><i class="fa fa-twitter"></i></a> 
+                                  <!-- whatsapp -->
                                   <a class="whatsapp" rel="noopener noreferrer" target="_blank" title="share on whatsapp" href="https://web.whatsapp.com:/send?text={{route('web.single-blog',['slug'=>$blog->slug,'b_id'=>$blog->id])}}&" data-text="{{$blog->title}}" data-href="{{route('web.single-blog',['slug'=>$blog->slug,'b_id'=>$blog->id])}}&"><i class="fa fa-whatsapp"></i></a>
                                   
                                 </div>
@@ -66,13 +66,3 @@
     @section('script')
 
     @endsection 
-    {{-- 
-      
-                                  <!-- -->
-      
-      <a class="facebook" href="https://www.facebook.com/share.php?u={{url}}&title={{title}}" target="blank"><i class="fa fa-facebook"></i></a>
-
-  <!-- twitter -->
-  <a class="twitter" href="https://twitter.com/intent/tweet?status={{title}}+{{url}}" target="blank"><i class="fa fa-twitter"></i></a>
-  <a rel="noopener noreferrer" target="_blank" title="share on whatsapp" href="https://web.whatsapp.com:/send?text=Ayush%20ministry%20allows%20sale%20of%20Patanjali%27s%20Coronil%20as%20immunity%20booster%20https%3A%2F%2Fwww.indiatoday.in%2Findia%2Fstory%2Fayush-ministry-allows-sale-of-patanjali-s-coronil-as-immunity-booster-1695911-2020-07-01%3Futm_content%3D%26utm_term%3D%20%0A%0ADownload%20the%20India%20Today%20app%20now%20to%20read%20our%20latest%20stories.%0Ahttps%3A%2F%2Findiatoday.app.link%2FTrK1ggqhLT" data-text="Ayush ministry allows sale of Patanjali's Coronil as immunity booster" data-href="https://www.indiatoday.in/india/story/ayush-ministry-allows-sale-of-patanjali-s-coronil-as-immunity-booster-1695911-2020-07-01?utm_content=&amp;utm_term="><i class="fa fa-whatsapp"></i></a>
-  --}}
